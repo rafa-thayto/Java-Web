a8c8020f01855b79699f37c83b11d63e2941a99e
Rafael Thayto <rafa.thayto@gmail.com>
2018-03-19 00:55:08 +0000

    Update user controller

--- TecNow/src/br/com/senai/sp/informatica/tecnow/controller/UserController.java
+++ TecNow/src/br/com/senai/sp/informatica/tecnow/controller/UserController.java
@@ -1,6 +1,10 @@
 package br.com.senai.sp.informatica.tecnow.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +32,26 @@ public class UserController {
 	}
 	
 	@PostMapping("/auth")
-	public String auth(User user) {
+	public String auth(User user, Model model) {
+		
+		// Store errors
+		List<String> errors = new ArrayList<>(10);
+		
+		if (user.getEmail() == null ||
+			user.getEmail().length() < 5 ||
+			user.getEmail().length() > 120) {
+			errors.add("O email é obrigatório e deve conter de 5 a 120 caracteres!");
+		}
+		if (user.getPassword() == null) {
+			errors.add("A senha é obrigatória!");
+		}
+		if (!errors.isEmpty()) {
+			System.out.println("Erros: " + errors);
+			model.addAttribute("errors", errors);
+			return "usuario/login";
+		}
+		// End
+		
 		return "redirect:app/";
 	}
 	
